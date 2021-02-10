ab86ef541c752217a891c1b2f19d4bf3f2712d89
Dan Buckland <danbucklan@gmail.com>
2021-02-10 16:48:52 +0000

    Finish adding local and prod webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,33 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  mode: 'none',
-  entry: './src/index.js',
+  mode: "none",
+  entry: "./src/index.js",
   output: {
-    path: path.join(__dirname, 'public/scripts')
+    path: path.join(__dirname, "public/scripts"),
   },
   module: {
-    rules: [{
-      test: /\.mjs$|\.js$/,
-      exclude: /node_modules/,
-      use: {
-        loader: 'babel-loader',
-        options: {
-          presets: ['@babel/preset-env']
-        }
-      }
-    }, {
-      test: /\.css$/,
-      use: ['style-loader', 'css-loader'],
-      exclude: [
-        /node_modules/
-      ]
-    }]
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+        exclude: [/node_modules/],
+      },
+    ],
   },
-  devtool: 'eval-cheap-module-source-map',
   devServer: {
-    contentBase: path.join(__dirname, 'public'),
-    publicPath: '/scripts/'
-  }
+    contentBase: path.join(__dirname, "public"),
+    publicPath: "/scripts/",
+  },
+  devtool: "source-map",
 };
