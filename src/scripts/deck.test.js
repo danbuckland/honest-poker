fb0f64099dc332723ebb50a9c7eb67d1a3181038
Dan Buckland <danbucklan@gmail.com>
2021-02-11 17:07:40 +0000

    Add a test for the draw method

--- src/scripts/deck.test.js
+++ src/scripts/deck.test.js
@@ -1,4 +1,5 @@
 import Deck from "./deck"
+import Card from "./card"
 
 test("should contain 52 cards", () => {
   const deck = new Deck()
@@ -7,7 +8,7 @@ test("should contain 52 cards", () => {
 
 test("each card should be unique", () => {
   const deck = new Deck()
-  
+
   const allCardsUnique = (cards) => {
     let cardCodes = {}
     for (const card of cards) {
@@ -21,4 +22,11 @@ test("each card should be unique", () => {
   }
 
   expect(allCardsUnique(deck.cards)).toBe(true)
+})
+
+test("should return an array containing 1 card when 1 is requested", () => {
+  const deck = new Deck()
+  const drawnCards = deck.draw(1)
+  expect(drawnCards.length).toBe(1)
+  expect(drawnCards[0]).toBeInstanceOf(Card)
 })
