884b572d45f7363d569875b4e662e4349ecdeb8f
Dan Buckland <danbucklan@gmail.com>
2021-02-12 17:20:36 +0000

    Start building hand ranking code and pass two tests

--- src/scripts/hand.js
+++ src/scripts/hand.js
@@ -11,6 +11,7 @@ export default class Hand {
     'Straight Flush',
     'Royal Flush',
   ]
+
   constructor(...cards) {
     this.cards = [...cards]
   }
@@ -23,23 +24,49 @@ export default class Hand {
     this.cards = []
   }
 
+  // Takes between 1 and 7 cards and returns the name of the best hand
   bestHand() {
-    //
-    return 'Unknown'
+    return this.rankings[this.getRanking()]
   }
 
-  containsPair() {
-    if (this.cards.length < 2) {
-      return false
-    }
-    let valuesCount = {}
-    for (const card of this.cards) {
-      if (valuesCount[card.value] === 1) {
-        return true
+  // Takes between 2 and 5 cards and returns the rank value of the hand
+  getRanking() {
+    const cardCounts = this.#getCounts()
+    const hasFlush = Object.keys(cardCounts.suits).length === 1
+    if (hasFlush && cardCounts.hasStraight) {
+      console.log(cardCounts.highCard)
+      if (cardCounts.highCard === 14) {
+        return 9
       } else {
-        valuesCount[card.value] = 1
+        return 8
       }
     }
-    return false
+  }
+
+  #getCounts() {
+    const cardCounter = { suits: {} }
+    const values = []
+
+    this.cards.forEach((card) => {
+      cardCounter[card.value] = ++cardCounter[card.value] || 1
+      cardCounter['suits'][card.suit] = ++cardCounter['suits'][card.suit] || 1
+      values.push(card.value)
+    })
+
+    cardCounter['hasStraight'] = this.#hasStraight(values)
+    cardCounter['highCard'] = Math.max(...values)
+
+    return cardCounter
+  }
+
+  #hasStraight(values) {
+    if (values.length !== 5) return false
+    values.sort()
+    for (let i = values.length - 1; i > 0; i--) {
+      if (values[i] - values[i - 1] !== 1) {
+        return false
+      }
+    }
+    return true
   }
 }
