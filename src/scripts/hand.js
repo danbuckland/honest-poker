9f5588e30586e6f46dea9aa39749fc16c98d2aa2
Dan Buckland <danbucklan@gmail.com>
2021-02-15 13:57:19 +0000

    Add logic to pass hand ranking tests with a mystery

--- src/scripts/hand.js
+++ src/scripts/hand.js
@@ -1,7 +1,7 @@
 export default class Hand {
   rankings = [
     'High Card',
-    'One Pair',
+    'Pair',
     'Two Pair',
     'Three of a Kind',
     'Straight',
@@ -33,22 +33,81 @@ export default class Hand {
   getRanking() {
     const cardCounts = this.#getCounts()
     const hasFlush = Object.keys(cardCounts.suits).length === 1
+
+    // Royal Flush or Straight Flush
     if (hasFlush && cardCounts.hasStraight) {
-      console.log(cardCounts.highCard)
       if (cardCounts.highCard === 14) {
         return 9
       } else {
+        // Code can never reach this line!
         return 8
       }
     }
+    // Four of a Kind
+    const matches = {
+      fourOfAKind: false,
+      threeOfAKind: false,
+      pairs: 0,
+    }
+    for (const value in cardCounts.values) {
+      if (cardCounts.values[value] === 4) {
+        matches.fourOfAKind = true
+      } else if (cardCounts.values[value] === 3) {
+        matches.threeOfAKind = true
+      } else if (cardCounts.values[value] === 2) {
+        matches.pairs++
+      }
+    }
+
+    if (matches.fourOfAKind) {
+      return 7
+    }
+
+    if (matches.threeOfAKind && matches.pairs) {
+      return 6
+    }
+
+    if (hasFlush) {
+      return 5
+    }
+
+    if (cardCounts.hasStraight) {
+      return 4
+    }
+
+    if (matches.threeOfAKind) {
+      return 3
+    }
+
+    if (matches.pairs === 2) {
+      return 2
+    }
+
+    if (matches.pairs === 1) {
+      return 1
+    }
+
+    return 0
   }
 
+  // Four of a kind
+  // {
+  //   suits: { Clubs: 1, Spades: 2, Diamonds: 1, Hearts: 1 },
+  //   values: { '13': 1, '14': 4 },
+  //   hasStraight: false,
+  //   highCard: 14
+  // }
+
   #getCounts() {
-    const cardCounter = { suits: {} }
+    const cardCounter = {
+      suits: {},
+      values: {},
+    }
     const values = []
 
     this.cards.forEach((card) => {
-      cardCounter[card.value] = ++cardCounter[card.value] || 1
+      cardCounter['values'][card.value] =
+        ++cardCounter['values'][card.value] || 1
       cardCounter['suits'][card.suit] = ++cardCounter['suits'][card.suit] || 1
       values.push(card.value)
     })
